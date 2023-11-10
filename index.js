120585e4cfebb0207fddb88bc37d5c090a8976d6
Luis Felipe Velasquez Garcia <pipevelasquez7@gmail.com>
2023-11-10 14:02:19 -0500

    replaced arrays by file system

--- index.js
+++ index.js
@@ -5,63 +5,142 @@ const initialProducts = [
     { title: "forth item", description: "Product 4", price: 800, thumbail: "null", code: "KLM", id: "", stock: "" },
 ];
 
-class ProductManager {
-    constructor(products) {
-        console.log("Product manager initialized");
-        this.products = products;
-        this.productArray = [];
 
-        /*   //Add all products to the products array on construct
-           this.products.forEach(product => {
-               this.addProduct(product);
-           });*/
+const fs = require('fs')
+const crypto = require('crypto');
+
+class ProductManager {
+    constructor(path) {
+        console.log("Product manager initialized");
+
+        this.path = path;
+
+        // Read initial products from file or create an empty array
+        this.productArray = this.readProductsFromFile() || [];
+
+        // Add all initial products to the products array
+        this.addProducts(initialProducts);
     }
 
+    // Hashing function for generating the product id
     generateID(product) {
-        //Generate an ID for the product by concatenatinc it's name, with it's index on the array and it's price
-        product.id = (product.title.replace(" ", "") + this.productArray.indexOf(product) + product.price).toUpperCase();
+        const idData = `${product.title}${product.price}${product.code}${product.description}${Math.random()}`;
+        const hash = crypto.createHash('md5').update(idData).digest('hex');
+        product.id = hash.toUpperCase();
     }
 
     validateProduct(product) {
-        // Add a product to the product array
-        if (!this.productArray.some(prod => prod.code === product.code)) {
-            this.productArray.push(product);
-            this.generateID(product)
-        } else {
-            console.log(`the product "${product.title}" (with code ${product.code}) already exists`)
-        }
+        // Check if the product with the same code or ID already exists
+        this.generateID(product);
+
+        const isCodeDuplicate = this.productArray.some(prod => prod.code === product.code);
+        const isIDDuplicate = this.productArray.some(prod => prod.id === product.id);
+
+        return !isCodeDuplicate && !isIDDuplicate;
+    }
+
+    addProducts(data) {
+        data.forEach(product => {
+            this.addProduct(product);
+        });
     }
 
     addProduct(product) {
-        //Validate products before adding them to the products array.
-        this.validateProduct(product);
+        // Validate and add product
+        if (this.validateProduct(product)) {
+            this.generateID(product);
+            this.productArray.push(product);
+            this.writeProductsToFile();
+        } else {
+            console.log(`The product "${product.title}" (with code ${product.code}) and ID ${product.id} already exists`);
+        }
+    }
+
+    readProductsFromFile() {
+        try {
+            const data = fs.readFileSync(this.path, 'utf-8');
+            return JSON.parse(data);
+        } catch (error) {
+            // Handle file read error or empty file
+            console.error('Error reading products file:', error.message);
+            return null;
+        }
+    }
+
+    writeProductsToFile() {
+        try {
+            fs.writeFileSync(this.path, JSON.stringify(this.productArray, null, 2), 'utf-8');
+        } catch (error) {
+            console.error('Error writing products file:', error.message);
+        }
     }
 
     getProducts() {
-        //Returns all products,
-        console.log(this.productArray);
+        const products = this.readProductsFromFile() || [];
+        console.log(products);
     }
 
-    getProductByID(id) {
-        const foundProduct = this.productArray.find(prod => prod.id === id);
-        if (foundProduct) {
-            console.log("Product found:", foundProduct.id);
+    updateProduct(id, data) {
+        let productIndex = this.productArray.findIndex(prod => prod.id === id);
+
+        if (productIndex !== -1) {
+            // Update the found product with the new data
+            this.productArray[productIndex] = { ...this.productArray[productIndex], ...data, id };
+            this.writeProductsToFile(); // Update the file after modifying the array
         } else {
             console.log("Product not found");
         }
     }
+
+    deleteProductByID(id) {
+        const productIndex = this.productArray.findIndex(prod => prod.id === id);
+
+        if (productIndex !== -1) {
+            // Remove the product from the array
+            this.productArray.splice(productIndex, 1);
+            this.writeProductsToFile(); // Update the file after modifying the array
+            console.log(`Product with ID ${id} deleted successfully.`);
+        } else {
+            console.log("Product not found.");
+        }
+    }
+
+    getProductByID(id) {
+        try {
+            const products = this.readProductsFromFile() || [];
+            const foundProduct = products.find(prod => prod.id === id);
+
+            if (foundProduct) {
+                console.log(`The product with ID ${id} is present (${foundProduct.title})`);
+                return foundProduct;
+            } else {
+                console.log("Product not found");
+                return null;
+            }
+        } catch (error) {
+            console.error('Error reading products file:', error.message);
+            return null;
+        }
+    }
 }
 
-const productManager = new ProductManager(initialProducts);
+const productManager = new ProductManager('./products.json');
 
-//Returns an empty array
+//Returns an empty array if products.json is empty
 productManager.getProducts();
 
 //Adds a product
-productManager.addProduct({ title: "fifth item", description: "Product 5", price: 1000, thumbail: "null", code: "NÑO", id: "", stock: "" })
+//productManager.addProduct({ title: "first item", description: "Product 5", price: 1000, thumbail: "null", code: "NÑO", id: "", stock: "" })
 
 //Returns an array with the added product
 productManager.getProducts();
 
-//PD: el id que esta puesta estara en la lista de productos, para que retorne falso, cambialo por cualquier otro texto
-productManager.getProductByID("THIRDITEM2600");
+//PD LOS IDS SERAN GENERADOS AUTOMATICAMENTE PARA CADA PRODUCTO LA PRIMERA VEZ QUE SE CORRA EL CODIGO, REVISAR EL ARCHIVO products.json Y REEMPLAZAR SEGUN CORRESPONDA
+
+
+//PD: actualiza un producto que exista en el archivo products.json por ID con un nuevo objeto (conservando el ID) 
+productManager.updateProduct("420906297FE196CB968C67471A43023B", { title: "Updated item", description: "update", price: 10000, thumbail: "null", code: "AABB", stock: "" })
+//PD: reemplazar el id con cualquier ID generado en los productos dentro del archivo products.json
+productManager.getProductByID("420906297FE196CB968C67471A43023B")
+
+
